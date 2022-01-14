a796486064f27908d1adf18ef78e566d4d3f22d1
zheqiu <zheqiu@dian.so>
2022-01-14 10:00:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+import opener from 'opener'
+import {BaseCommand} from '@yarnpkg/cli';
+import { Option }  from 'clipanion';
+import axios from 'axios'
+
+class Docs extends BaseCommand {
+
+    static paths = [['docs']]
+
+    packages = Option.String()
+    async execute() {
+        if (!this.packages) return
+        const list = this.packages.split(',')
+        list.forEach(item => {
+            opener(`https://www.npmjs.com/package/${item}`) 
+        })
+    }
+}
+
+class View extends BaseCommand {
+    static paths = [['view']]
+
+    info = Option.String()
+    async execute() {
+       const [packageName, version] = this.info.split('@')
+       const res =  await axios.get(`http://registry.yarnpkg.com/${packageName}/${version || ''}`)
+       console.log(res.data, 'res')
+    }
+}
+
+export default {
+    commands: [
+        Docs,
+        View
+    ]
+}
