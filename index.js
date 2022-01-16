db124bb84730eb69a58f50176c6f46a6c6157624
zheqiu <zheqiu@dian.so>
2022-01-16 17:33:39 +0800

    chore:优化docs逻辑

--- index.js
+++ index.js
@@ -1,30 +1,102 @@
 import opener from 'opener'
-import {BaseCommand} from '@yarnpkg/cli';
-import { Option }  from 'clipanion';
+import {BaseCommand} from '@yarnpkg/cli'
+import { Option, Command }  from 'clipanion'
 import axios from 'axios'
+import semver from 'semver'
+import hostedGitInfo from 'hosted-git-info'
+
+
+function hostedFromMani(mani) {
+    const r = mani.repository
+    const rurl = !r ? null
+      : typeof r === 'string' ? r
+      : typeof r === 'object' && typeof r.url === 'string' ? r.url
+      : null
+  
+    return (rurl && hostedGitInfo.fromUrl(rurl.replace(/^git\+/, ''))) || null
+}
 
 class Docs extends BaseCommand {
 
     static paths = [['docs']]
 
+    static usage = Command.Usage({
+        category: `docs commands`,
+        description: `open the package docs page`,
+        details: `
+        打开包对应的文档地址
+        `,
+        examples: [[
+          `Open the search window`,
+          `yarn search`,
+        ]],
+      });
+
     packages = Option.String()
     async execute() {
         if (!this.packages) return
         const list = this.packages.split(',')
-        list.forEach(item => {
-            opener(`https://www.npmjs.com/package/${item}`) 
-        })
+        await Promise.all(list.map(pkg => this.getDocs(pkg)))
+    }
+
+    async getDocs (pkg) {
+        const res = await axios(`http://registry.yarnpkg.com/${pkg}`)
+        const pckmnt = res.data
+        const url = this.getDocsUrl(pckmnt)
+        await opener(url)
+    }
+    
+    getDocsUrl (mani) {
+        if (mani.homepage) {
+            return mani.homepage
+        }
+
+        const info = hostedFromMani(mani)
+        if (info) {
+            return info.docs()
+        }
+
+        return 'https://www.npmjs.com/package/' + mani.name
     }
 }
 
 class View extends BaseCommand {
     static paths = [['view']]
 
-    info = Option.String()
+    identifier = Option.String()
+    infoKey = Option.String()
     async execute() {
-       const [packageName, version] = this.info.split('@')
-       const res =  await axios.get(`http://registry.yarnpkg.com/${packageName}/${version || ''}`)
-       console.log(res.data, '----')
+        const [packageName, version] = this.identifier.split('@')
+        const res =  await axios.get(`http://registry.yarnpkg.com/${packageName}`)
+        const pckmnt = res.data
+        const data = version ? pckmnt.versions[version] : pckmnt
+        const versions = Object.keys(pckmnt.versions || []).sort(semver.compareLoose)
+        data.versions = versions
+        if (pckmnt['dist-tags']) {
+            Object.keys(pckmnt['dist-tags']).forEach(key => {
+                data[key] = pckmnt['dist-tags'][key]
+            })
+        }
+        const infoKeys = this.infoKey?.split('.')
+        const info = infoKeys ? formatInfo(data, infoKeys) : data
+        loading.stop()
+        if (!info) return
+        try {
+            if (typeof info === 'string') {
+                console.log(info)
+            } else {
+                console.log(JSON.stringify(info, null, 2))
+            }
+        } catch(err) {
+            console.log(err.message)
+        }
+
+        function formatInfo(obj, keys) {
+          const res = keys.reduce((pre, next) => {
+            return pre ? pre[next] : pre
+          }, obj)
+          return res
+        }
     }
 }
 
